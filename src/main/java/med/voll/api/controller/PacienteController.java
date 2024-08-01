75dab2bcb86b284bb55ab4d13040f0f330ba4e50
Gustavo Freire Pereira <gustavo_freire18@usp.br>
2024-07-31 22:50:53 -0300

    commit paciente

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,10 +1,27 @@
 package med.voll.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+import med.voll.api.dto.paciente.PacienteDtoCadastro;
+import med.voll.api.modelo.Paciente;
+import med.voll.api.repository.PacienteRepository;
 
 @RestController
 @RequestMapping("/pacientes")
 public class PacienteController {
 
+    @Autowired
+    private PacienteRepository repository;
+
+    @PostMapping
+    @Transactional
+    public void cadastrar(@RequestBody @Valid PacienteDtoCadastro dto) {
+        repository.save(new Paciente(dto));
+    }
 }
---
+++ src/main/java/med/voll/api/modelo/Paciente.java
@@ -0,0 +1,36 @@
+package med.voll.api.modelo;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import med.voll.api.dto.medico.MedicoDtoCadastro;
+import med.voll.api.dto.paciente.PacienteDtoCadastro;
+
+@Entity(name = "Paciente")
+@Table(name = "pacientes")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Paciente {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String telefone;
+    private String cpf;
+
+    @Embedded
+    private Endereco endereco;
+
+    public Paciente(PacienteDtoCadastro dto) {
+        this.nome = dto.nome();
+        this.email = dto.email();
+        this.telefone = dto.telefone();
+        this.cpf = dto.cpf();
+        this.endereco = new Endereco(dto.endereco());
+    }
+}
